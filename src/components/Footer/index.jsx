e7c926a1bae78ec7ef1ddc5857b35da41864d9b0
Lucas Smaniotto <lucas91755@gmail.com>
2023-01-25 08:32:13 -0300

    style: melhorando o visual do footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,15 @@
 import styles from './Footer.module.css'
+import { AiOutlineTwitter, AiFillGithub, AiOutlineInstagram } from 'react-icons/ai';
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-        <p>Desevolvido por <a href="https://github.com/lucassmaniotto" target="_blank" className={styles.ancor}>Lucas Smaniotto</a></p>
+        <p>Desevolvido por <a href="https://github.com/lucassmaniotto" className={styles.ancor}>Lucas Smaniotto</a></p>
+        <div className={styles.socialLinks}>
+            <a href="https://github.com/lucassmaniotto"><AiFillGithub className={styles.socialIcons} /></a>
+            <a href="https://www.instagram.com/corvus_tenebrae"><AiOutlineInstagram className={styles.socialIcons} /></a>
+            <a href="https://twitter.com/alpiste_punk"><AiOutlineTwitter className={styles.socialIcons} /></a>
+        </div>
     </footer>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,8 @@ import Footer from 'components/Footer'
 import Header from 'components/Header'
 import React from 'react'
 
+//import styles from './Home.module.css'
+
 export default function Home() {
   return (
     <>
